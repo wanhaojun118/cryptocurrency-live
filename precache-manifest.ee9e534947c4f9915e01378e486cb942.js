7020125240ad5af4a2ae62153133f275d14e18c9
Wan Hao Jun <haojun@wtech.software>
2020-02-01 15:51:43 +0800

    Updates

---
+++ precache-manifest.ee9e534947c4f9915e01378e486cb942.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1623ac86f92f80da49855adbb1cabaa7",
+    "url": "/cryptocurrency-live/index.html"
+  },
+  {
+    "revision": "8bf90b78090f1838155b",
+    "url": "/cryptocurrency-live/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dfbf8abafaa99b5d9a12",
+    "url": "/cryptocurrency-live/static/css/main.d841ca6c.chunk.css"
+  },
+  {
+    "revision": "8bf90b78090f1838155b",
+    "url": "/cryptocurrency-live/static/js/2.52f9c0e9.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/cryptocurrency-live/static/js/2.52f9c0e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfbf8abafaa99b5d9a12",
+    "url": "/cryptocurrency-live/static/js/main.7763b963.chunk.js"
+  },
+  {
+    "revision": "129ec2554235342b5551",
+    "url": "/cryptocurrency-live/static/js/runtime-main.2fdae210.js"
+  },
+  {
+    "revision": "4cfd3f8b0c4f50822987abb586b9122a",
+    "url": "/cryptocurrency-live/static/media/BTC.4cfd3f8b.png"
+  },
+  {
+    "revision": "5d57f92bb1e56a6b7ab709b69474265a",
+    "url": "/cryptocurrency-live/static/media/DASH.5d57f92b.png"
+  },
+  {
+    "revision": "6594b16f07c7c7491fe1336a1034c9dd",
+    "url": "/cryptocurrency-live/static/media/ETH.6594b16f.png"
+  },
+  {
+    "revision": "b074f6be8fad7ea7c211e070eb46639b",
+    "url": "/cryptocurrency-live/static/media/dark-theme.b074f6be.jpg"
+  }
+]);
